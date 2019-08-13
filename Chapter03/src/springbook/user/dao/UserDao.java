5b9cd4c8ede1804bdc9f9e52dfe2d7f4b154d849
marunoona <ye_jin92@naver.com>
2019-08-13 18:22:43 +0900

    Chapter03 추가

---
+++ Chapter03/src/springbook/user/dao/UserDao.java
@@ -0,0 +1,101 @@
+package springbook.user.dao;
+
+import springbook.user.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private DataSource dataSource;
+
+    // 수정자를 이용한 의존관계 주입 - datasource 사용
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void addUser(User user) throws SQLException {
+        //Connection connection = this.connectionMaker.makeConnection();
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = this.dataSource.getConnection();
+
+            ps = connection.prepareStatement(
+                    "insert into users(id, name, password) values(?,?,?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if(ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public User getUser(String id) throws SQLException {
+        //Connection connection = this.connectionMaker.makeConnection();
+        Connection connection = this.dataSource.getConnection();
+
+        PreparedStatement ps = connection.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        connection.close();
+
+        return user;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException  {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
+}
---
+++ Chapter03/src/springbook/user/dao/UserDaoTest.java
@@ -0,0 +1,105 @@
+package springbook.user.dao;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * UserDaoTest는 UserDao와 ConnectionMaker 구현 클래스와의
+ * 런타임 오브젝트 의존 관례를 설정하는 책임을 담당해야 한다.
+ * <p>
+ * UserDao가 사용할 ConnectionMaker 클래스를 선정하는 책임
+ * 이후 이 역할을 Factory가 할 수 있도록 수정한다.
+ */
+
+//스프링의 테스트 컨텍스트 프레임워크의 Junit 확장 가능 지정
+@RunWith(SpringJUnit4ClassRunner.class)
+//테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트 위치 지정
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class UserDaoTest {
+
+    //테스트 오브젝트가 마늘어지고 나면
+    //스트링 테스트 컨텍스트에 의해 자동으로 값이 주입된다.
+    @Autowired
+    private ApplicationContext context;
+
+    //애플리케이션 컨텍스트가 갖고 잇는 빈을 DI받는 것
+    @Autowired
+    private UserDao userDao;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+    /**
+     * 중복 코드를 넣을 메소드
+     * @Test가 실행되기 전에 먼저 실행되어야 하는 메소드를 정의
+     */
+    @Before
+    public void setUp(){
+        //ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        //this.userDao = context.getBean("userDao", UserDao.class);
+
+        this.user1 = new User("gyumee", "박성철", "springno1");
+        this.user2 = new User("leegw700", "이길원", "springno2");
+        this.user3 = new User("bumjin", "박범진", "springno3");
+    }
+
+
+    @Test
+    public void addAndGetTest() throws SQLException {
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        UserDao userDao = context.getBean("userDao", UserDao.class);
+//
+//        User user1 = new User("apple", "곽수아", "jejudo");
+//        User user2 = new User("dlwldms", "이지은", "marado");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), is(0));
+
+        userDao.addUser(user1);
+        userDao.addUser(user2);
+        assertThat(userDao.getCount(), is(2));
+
+        User userget1 = userDao.getUser(user1.getId());
+        assertThat(userget1.getName(), CoreMatchers.is(user1.getName()));
+        assertThat(userget1.getPassword(), CoreMatchers.is(user1.getPassword()));
+
+        User userget2 = userDao.getUser(user2.getId());
+        assertThat(userget2.getName(), CoreMatchers.is(user2.getName()));
+        assertThat(userget2.getPassword(), CoreMatchers.is(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException {
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//
+//        UserDao userDao = context.getBean("userDao", UserDao.class);
+//        User user1 = new User("gyumee", "박성철", "springno1");
+//        User user2 = new User("leegw700", "이길원", "springno2");
+//        User user3 = new User("bumjin", "박범진", "springno3");
+
+        userDao.deleteAll();
+        assertThat(userDao.getCount(), CoreMatchers.is(0));
+
+        userDao.addUser(user1);
+        assertThat(userDao.getCount(), CoreMatchers.is(1));
+
+        userDao.addUser(user2);
+        assertThat(userDao.getCount(), CoreMatchers.is(2));
+
+        userDao.addUser(user3);
+        assertThat(userDao.getCount(), CoreMatchers.is(3));
+    }
+}
