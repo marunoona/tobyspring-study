52d02cb95a87cb735614071816beb6941e94d16e
marunoona <ye_jin92@naver.com>
2019-08-15 13:38:34 +0900

    사용자 서비스 추가 의존주입 관계 수정

---
+++ Chapter05/src/springbook/user/service/UserServiceTest.java
@@ -0,0 +1,66 @@
+package springbook.user.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserDao userDao;
+
+    List<User> users;    // test fixture
+
+    @Before
+    public void setUp() {
+        //배열을 리스트로 만들어주는 메소드 이용
+        //배열을 가변인자로 넣어주면 더욱 편리함
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승한", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100, 100)
+        );
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+        for(User user : users)
+            userDao.addUser(user);
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    /**
+     * DB에서 사용자 정보를 가져와 레벨을 확인하는 코드
+     * @param user
+     * @param expectedLevel
+     */
+    private void checkLevel(User user, Level expectedLevel){
+        User userUpdate = userDao.getUser(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+}
