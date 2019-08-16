c82c8a0dbbaa18084ca2023697e0f33c1543bfed
marunoona <ye_jin92@naver.com>
2019-08-17 01:03:48 +0900

    MockObject 생성 및 테스트 확인

--- Chapter05/src/springbook/user/service/UserServiceTest.java
+++ Chapter05/src/springbook/user/service/UserServiceTest.java
@@ -4,7 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -12,6 +15,7 @@ import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -112,8 +116,9 @@ public class UserServiceTest {
     }
 
     @Test
+    @DirtiesContext
     public void upgradeAllOrNothing() {
-        UserService testUserService = new TestUserService(users.get(3).getId());
+        /*UserService testUserService = new TestUserService(users.get(3).getId());
         testUserService.setUserDao(this.userDao);
         testUserService.setTransactionManager(this.transactionManager);
         testUserService.setMailSender(this.mailSender);
@@ -121,13 +126,39 @@ public class UserServiceTest {
         userDao.deleteAll();
         for (User user : users) userDao.addUser(user);
 
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
         try {
             testUserService.upgradeLevels();
             fail("TestUserServiceException expected");
         } catch (TestUserServiceException e) {
         }
 
-        checkLevelUpgraded(users.get(1), false);
+        checkLevelUpgraded(users.get(1), false);*/
+
+        userDao.deleteAll();
+        for (User user : users) userDao.addUser(user);
+
+        //메일 발송 결과를 테스트할 수 있도록 목 오브젝트를 만들어
+        //UserService의 의존 오브젝트로 주입해준다.
+        MockMailSender mockMailSender = new MockMailSender();
+        userService.setMailSender(mockMailSender);
+
+        //업그레이드 테스트. 메일 발송이 일어나면
+        //MockMailSender 오브젝트의 리스트에 그 결과가 저장된다.
+        userService.upgradeLevels();
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);
+
+        //목 오브젝트에 저장된 메일 수신자 목록을 가져와서
+        //업그레이드 대상과 일치하는지 확인한다.
+        List<String> request = mockMailSender.getRequest();
+        assertThat(request.size(), is(2));
+        assertThat(request.get(0), is(users.get(1).getEmail()));
+        assertThat(request.get(1), is(users.get(3).getEmail()));
     }
 
 
@@ -145,5 +176,27 @@ public class UserServiceTest {
     }
 
     static class TestUserServiceException extends RuntimeException {
+    }
+
+    static class MockMailSender implements MailSender {
+
+        //UserService로부터 전송 요청을 받은 메일 주소를
+        //저장해두고 이를 읽을 수 있게 한다.
+        private List<String> request = new ArrayList<>();
+
+        public List<String> getRequest() {
+            return request;
+        }
+
+        @Override
+        public void send(SimpleMailMessage simpleMailMessage) throws MailException {
+            //전송 요청을 받은 이메일 주소를 저장해준다. 간단하게 첫 번재 수신자의 메일 주소만 저장
+            request.add(simpleMailMessage.getTo()[0]);
+        }
+
+        @Override
+        public void send(SimpleMailMessage... simpleMailMessages) throws MailException {
+
+        }
     }
 }
