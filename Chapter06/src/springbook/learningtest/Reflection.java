c6e0e7a0531e58ef0fe46e54058e87d6d84395bc
marunoona <ye_jin92@naver.com>
2019-08-18 17:20:44 +0900

    리플렉션 테스트

---
+++ Chapter06/src/springbook/learningtest/Reflection.java
@@ -0,0 +1,33 @@
+package springbook.learningtest;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class Reflection {
+    @Test
+    public void invokeMethod() throws Exception {
+        String name = "Spring";
+
+        // length
+        assertThat(name.length(), is(6));
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat((Integer)lengthMethod.invoke(name), is(6));
+
+        // toUpperCase
+        assertThat(name.charAt(0), is('S'));
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat((Character)charAtMethod.invoke(name, 0), is('S'));
+    }
+
+    @Test
+    public void createObject() throws Exception {
+        Date now = (Date) Class.forName("java.util.Date").newInstance();
+    }
+}
